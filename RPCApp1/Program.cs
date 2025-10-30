27aea0d8c6b5ba357f7bf13714ad740176bdedb3
ubyerS <125369545+ubyerS@users.noreply.github.com>
2025-10-30 13:44:03 +0300

    Add files via upload

---
+++ RPCApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+class Program
+{
+    static List<int> FindDuplicatesOptimized(int[] arr)
+    {
+        HashSet<int> seen = new HashSet<int>();
+        HashSet<int> duplicates = new HashSet<int>();
+
+        foreach (var num in arr)
+        {
+            if (!seen.Add(num))
+            {
+                duplicates.Add(num);
+            }
+        }
+
+        return new List<int>(duplicates);
+    }
+
+    static void Main()
+    {
+        int[] testArray = { 1, 2, 3, 2, 4, 5, 5, 6 };
+
+        Stopwatch sw = Stopwatch.StartNew();
+        var duplicates = FindDuplicatesOptimized(testArray);
+        sw.Stop();
+
+        Console.WriteLine("Дубликаты: " + string.Join(", ", duplicates));
+        Console.WriteLine("Время: " + sw.ElapsedTicks + " тиков");
+    }
+}
